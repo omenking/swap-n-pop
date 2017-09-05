4d7e19c0388f57f17cecce5684f66a86c48b9efa
Andrew Brown <andrew@monsterboxpro.com>
2017-09-04 20:29:28 -0400

    6x combo

--- app/spec/src/renderer/update_spec.js
+++ app/spec/src/renderer/update_spec.js
@@ -161,7 +161,7 @@ describe('panel_actions', function() {
          [0,10,2,STATIC,0,false], [1,10,3,STATIC,0,false], [2,10,0,STATIC,0,false])
   })
 
-  it('#clear', function(){
+  it('#combo_3x', function(){
     // N 2 N
     // 2 1 2
     // 3 1 3
@@ -182,6 +182,26 @@ describe('panel_actions', function() {
          [0,9 ,3,STATIC,0,F], [1,9 ,N,STATIC,0,F], [2,9 ,3,STATIC,0,F],
          [0,10,2,STATIC,0,F], [1,10,N,STATIC,0,F], [2,10,2,STATIC,0,F])
   })
+
+  it('#combo_6x', function(){
+    // 2 3
+    // 2 3
+    // 2 3
+    load([0,8 ,2,STATIC,0,F], [1,8 ,3,STATIC,0,F],
+         [0,9 ,2,STATIC,0,F], [1,9 ,3,STATIC,0,F],
+         [0,10,2,STATIC,0,F], [1,10,3,STATIC,0,F])
+    //################################################################
+    playfield.update()
+    chec([0,8 ,2,CLEAR,117,F], [1,8 ,3,CLEAR,117,F],
+         [0,9 ,2,CLEAR,117,F], [1,9 ,3,CLEAR,117,F],
+         [0,10,2,CLEAR,117,F], [1,10,3,CLEAR,117,F])
+    for(i = 0; i < 117; i++){ playfield.update() }
+    chec([0,8 ,N,STATIC,0,F], [1,8 ,N,STATIC,0,F],
+         [0,9 ,N,STATIC,0,F], [1,9 ,N,STATIC,0,F],
+         [0,10,N,STATIC,0,F], [1,10,N,STATIC,0,F])
+    playfield.score.should.eql(110)
+  })
+
   it('#chain', function(){
     // N 2 N
     // 2 N 2
@@ -204,6 +224,6 @@ describe('panel_actions', function() {
          [0,8 ,2,STATIC,0,F], [1,8 ,N,STATIC,0,F], [2,8 ,2,STATIC,0,F],
          [0,9 ,3,STATIC,0,F], [1,9 ,N,STATIC,0,F], [2,9 ,3,STATIC,0,F],
          [0,10,2,CLEAR,90,F], [1,10,2,CLEAR ,90,T],[2,10,2,CLEAR,90,F])
+    playfield.chain.should.eql(1)
   })
-
 }) //klass
