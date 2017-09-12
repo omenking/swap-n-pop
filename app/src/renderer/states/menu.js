0e125fa3fb78de52cc3713abd3c78316c61a9d45
Andrew Brown <andrew@monsterboxpro.com>
2017-09-11 23:29:42 -0400

    remove version text

--- app/src/renderer/states/menu.js
+++ app/src/renderer/states/menu.js
@@ -1,37 +1,19 @@
 module.exports = function(game){
   const APP = require('../../../app')('../../../')
-  const {VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH} = require(APP.path.core('data'))
   const ComponentMenu = require(APP.path.components('menu'))(game)
   const {ipcRenderer: ipc} = require('electron')
 
   class controller {
     constructor() {
       console.log('compiled states menu')
-      this.create = this.create.bind(this);
-      this.ver    = this.ver.bind(this);
-      this.update = this.update.bind(this);
-      this.menu   = new ComponentMenu();
-      return this
+      this.create = this.create.bind(this)
+      this.update = this.update.bind(this)
+      this.menu   = new ComponentMenu()
     }
     create() {
-      game.stage.backgroundColor = '#ffffff';
-
-      this.bg = game.add.tileSprite(0, 0, game.world.width, game.world.height, 'bg_blue');
-      this.menu.create();
-
-      return this.ver();
-    }
-    ver() {
-      const v = `${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}`;
-      const t = `VER ${v}`;
-      const build = game.add.text(16,16, t, {
-        font: '10px Verdana',
-        fill: '#999999',
-        fontWeight: 'bold',
-        align: 'center'
-      }
-      );
-      return build.anchor.setTo(0, 1);
+      game.stage.backgroundColor = '#ffffff'
+      this.bg = game.add.tileSprite(0, 0, game.world.width, game.world.height, 'bg_blue')
+      this.menu.create()
     }
     update() {
       game.controls.update()
