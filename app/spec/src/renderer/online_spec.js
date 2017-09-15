696617f6e9b92780d25e9d345e2be7898056a138
Andrew Brown <andrew@monsterboxpro.com>
2017-09-15 00:48:54 -0400

    Update online spec with more specificity

--- app/spec/src/renderer/online_spec.js
+++ app/spec/src/renderer/online_spec.js
@@ -13,11 +13,11 @@ const Stage1 = require(APP.path.states('mode_vs'))(game1)
 
 var _stage0 = null
 var _stage1 = null
-function update(){
+function update(p0,p1){
   _stage0.update()
   _stage1.update()
-  return [_stage0.inputs.last_pack,
-          _stage1.inputs.last_pack]
+  _stage0.inputs.last_pack.should.eql(p0)
+  _stage1.inputs.last_pack.should.eql(p1)
 }
 
 describe('Online Simulation', function() {
@@ -43,25 +43,35 @@ describe('Online Simulation', function() {
       //Buffer.from([0x04,0x01,0x00,0x00,[>*/0x00,/*<]0x00,0x00]),
   it('#data', function(){
     game0.controls.execute(0,0x01) // up
-    update().should.eql([
+    game1.controls.execute(0,0x08) // right
+    update(
       {ack0: 0, ack1: 0, frame_count: 2, frames: [0x00,0x01]},
-      {ack0: 0, ack1: 0, frame_count: 2, frames: [0x00,0x00]}
-    ]) //1
+      {ack0: 0, ack1: 0, frame_count: 2, frames: [0x00,0x08]}
+    ) //1
+    stage0.inputs.inputs[0].should.eql([0x00,0x01])
+    stage0.inputs.inputs[1].should.eql([0x00])
+    stage1.inputs.inputs[0].should.eql([0x00,0x08])
+    stage1.inputs.inputs[1].should.eql([0x00])
+
     game0.controls.execute(0,0x00) // none
-
     let pack0_2 = {ack0: 0, ack1: 0, frame_count: 3, frames: [0x00,0x01,0x00]}
-    let pack1_2 = {ack0: 0, ack1: 0, frame_count: 3, frames: [0x00,0x00,0x00]}
-    update().should.eql([pack0_2,pack1_2])//2
+    let pack1_2 = {ack0: 0, ack1: 0, frame_count: 3, frames: [0x00,0x08,0x08]}
+    update(pack0_2,pack1_2)
     stage0.inputs.unpack(pack1_2)
+    stage0.inputs.inputs[0].should.eql([0x00,0x01,0x00])
+    stage0.inputs.inputs[1].should.eql([0x00,0x08,0x08])
+    stage1.inputs.inputs[0].should.eql([0x00,0x08,0x08])
+    stage1.inputs.inputs[1].should.eql([0x00])
 
+    game1.controls.execute(0,0x00) // none
     let pack0_3 = {ack0: 0, ack1: 3, frame_count: 4, frames: [0x00,0x01,0x00,0x00]}
-    let pack1_3 = {ack0: 0, ack1: 0, frame_count: 4, frames: [0x00,0x00,0x00,0x00]}
-    update().should.eql([pack0_3,pack1_3]) //3
+    let pack1_3 = {ack0: 0, ack1: 0, frame_count: 4, frames: [0x00,0x08,0x08,0x00]}
+    update(pack0_3,pack1_3)
     stage1.inputs.unpack(pack0_3)
 
-    update().should.eql([
+    update(
       {ack0: 0, ack1: 3, frame_count: 5, frames: [0x00,0x01,0x00,0x00,0x00]},
       {ack0: 3, ack1: 4, frame_count: 2, frames: [0x00,0x00]}
-    ]) //4
+    )
   }) //it
 }) //describe
