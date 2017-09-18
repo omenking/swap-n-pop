0a11dacb2da2455202ef3bfff7af187e1549c705
Andrew Brown <andrew@monsterboxpro.com>
2017-09-17 20:12:34 -0400

    add debugging

--- app/src/renderer/states/mode_vs.js
+++ app/src/renderer/states/mode_vs.js
@@ -127,9 +127,9 @@ module.exports = function(game){
     }
     roll_to(from,to){
       if (from > to) { // rollback
-        //console.log('<-----')
+        console.log('<-----')
       } else { //rollforward
-        //console.log(`${from} ${to}----->`)
+        console.log(`${from} ${to}----->`)
         this.snapshots.load(from)
         // since we loaded a snapshot, maybe we don't need to step
         // throuh the frame we loaded the snapshot on.
