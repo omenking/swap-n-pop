9bf323ba3c5b138caf0fb205cc45a9bb25f2c26d
Skytrias <sotdead91@gmail.com>
2017-10-04 18:42:58 +0200

    Implemented proper control switching, added comments

--- app/src/renderer/components/menu_pause.js
+++ app/src/renderer/components/menu_pause.js
@@ -2,7 +2,11 @@ module.exports = function(game){
   const APP = require('../../../app')('../../../')
   const ComponentMenuPauseCursor = require(APP.path.components('menu_pause_cursor'))(game)
 
+  /** A Menu_pause handles the pausing of a stage
+   *  new controls are provided to move in the menu and use various options
+   */
   class controller {
+    /** bindings & new menu cursor created */
     constructor() {
       this.create = this.create.bind(this);
       this.cancel = this.cancel.bind(this);
@@ -13,46 +17,63 @@ module.exports = function(game){
       this.cursor = new ComponentMenuPauseCursor();
     }
 
+    /**
+     * pausing turned of normally
+     * add a sprite to this object but turn it off for now
+     * create a cursor with no controls provided yet 
+     * @param {mode_vs} stage reference to call both playfields 
+     */
     create(stage){
       this.paused = false;
+
       this.stage = stage;
 
       // for now playfield.x changed to a value
       this.sprite = game.add.sprite(100 + 4, 100, 'menu_pause');
       this.sprite.visible = false;
+
+      // create a controller with this menu given as a reference, push 2 methods of the menu
       return this.cursor.create(this, 8, 8, [
         this.continue,
         this.cancel
       ]);
     }
 
+    /** leave the game */
     cancel() {
-      this.paused = false
       game.state.start('menu')
     }
 
+    /** called through cursor, unpauses the menu and turns of the sprite again
+     * also calls the mode_vs||stage resume method which will return new cursor controls
+     */
     continue() {
       this.paused         = false;
       this.sprite.visible = false;
-
-      console.log("testing stage resume call inside menu");
-      
-      // fill with no player control
-      this.cursor.map_controls();
+      this.stage.resume();
     }
 
+    /** unpauses update method of the menu, 
+     * make this object visible and 
+     * reassigns controls to both playfields registered players */
     pause(){
       this.paused = true;
       this.sprite.visible = true;
-      
+
       // give player control
-      this.cursor.map_controls(this.stage.playfield1);
+      this.cursor.map_controls(this.stage.playfield1.pi);
+      // below possibly gives 2nd player controls of the menu (didnt test, but didnt break p1.pi control)
+      this.cursor.map_controls(this.stage.playfield2.pi);
     }
 
+    /** once unpaused the menu will update its cursor */
     update() {
       if (!this.paused) 
         return; 
 
+      console.log("update of menu called");
+
+      // check for any Input and if any pressed - continue or cancel may be called here
       this.cursor.update();
     }
   }
