db0a130d143f6a5b1d3fa5054b7ddb40cc82af42
Skytrias <sotdead91@gmail.com>
2017-10-07 01:39:11 +0200

    [F31] centralized stack algorithm and changed random gen to be seed based

--- app/src/renderer/core/stack.js
+++ app/src/renderer/core/stack.js
@@ -1,83 +1,108 @@
-module.exports = function(game){
-  const APP = require('../../../app')('../../../')
-  const {
-    COLS, ROWS, PANELS
-  } = require(APP.path.core('data'))
-  const _f = require(APP.path.core('filters'))
-  const ss = require('shuffle-seed')
-  class controller {
-    constructor(rng) {
-      this.rng     = rng
-      this.steps   = this.steps.bind(this)
-      this.step    = this.step.bind(this)
-      this.create  = this.create.bind(this)
-      this.layout  = this.layout.bind(this)
-      this.at      = this.at.bind(this)
-      this.matched = this.matched.bind(this)
-    }
-    create() {
-      this.panels = new Array(PANELS).fill(null)
-      this.steps(this.layout())
-    }
-    steps(layout){
-      for (let i = 0; i < layout.length; i++) {
-        this.step(layout,i)
+module.exports = function(game) {
+	const APP = require('../../../app')('../../../');
+	const {
+		COLS, 
+		ROWS, 
+		PANELS
+	} = require(APP.path.core('data'));
+	const _f = require(APP.path.core('filters'));
+  const ss = require('shuffle-seed');
+	
+	/** 
+	 * Creates a randomized 1d Array with numbers 
+	 * that can be assigned to a Stack to form all Sprites
+	 * The Algorithm creates the Numbers carefully so 
+	 * no combos are done once finished
+	 */
+	class Stack {
+		/**
+		 * Panels Array that saves the numbers being generated 
+		 * 
+		 * 
+		 * @param {object} rng seed that the randomness is based on 
+		 */
+		constructor(rng) {
+			this.create = this.create.bind(this);
+			this.logArrayLikeAPlayfield = this.logArrayLikeAPlayfield.bind(this);
+		
+			this.rng = rng;
+			this.panels = 0;
+		}
+
+		/**
+     * from top to bottom creation of all playfields stack sprites
+     * numbers are being generated - regenerated if recent numbers are the same
+     * @param {integer} range 
+     */
+		create(range, wellPercentage) {
+			this.panels = new Array(PANELS).fill(null);
+
+			var lastNumber = -1;
+			var amountOfSprites = [0, 1, 2, 3, 4];
+
+			var amountOfWellSpace = new Array(wellPercentage).fill(0);
+			amountOfWellSpace[0] = 1;
+
+      // fill with nulls so empty space will result in unvisible blocks
+      var savedNumbers = new Array(PANELS).fill(null);
+
+      for (var i = range * COLS; i < PANELS; i++) {
+        let currentNumber = ss.shuffle(amountOfSprites, this.rng())[0];
+
+        // x and y pos to move in the array and detect things
+        let indexes = _f.i2xy(i);
+
+        // save the top number, if top number is set the number below it should never be the same
+        let topNumber = savedNumbers[_f.xy2i(indexes[0], indexes[1] - 1)];
+
+				// random chance that currentnumber is simply null
+				if (ss.shuffle(amountOfWellSpace, this.rng())[0] == 1) 
+					currentNumber = null;
+				
+					// if x = 0 we dont need to detect the left neighbor color so we set last to something different
+				if (indexes[0] == 0) 
+					lastNumber == -1;
+
+				// generate new number until currentNumber is unique
+				// not when its already null, do when its top / last number is same
+				while (currentNumber != null &&
+								currentNumber == topNumber ||
+								currentNumber == lastNumber) {
+					currentNumber = ss.shuffle(amountOfSprites, this.rng())[0];
+				}
+
+				// set last number and send current to array
+        lastNumber = currentNumber;
+        savedNumbers[i] = currentNumber;
+			}
+
+			this.logArrayLikeAPlayfield(savedNumbers);
+
+			/* not currently working, gotta sleep
+			for (var i = PANELS; i > 0; i--) {
+				if (savedNumbers[i] == null) {
+					savedNumbers[i - COLS] = null;
+				}
+			}*/
+
+			this.panels = savedNumbers;
+		}
+
+		/**
+		 * logs the array created in a readable way for debugging purposes
+		 * @param {Array} arrayToLog 1d array of numbers/elements
+		 */
+    logArrayLikeAPlayfield(arrayToLog) {
+      for (var i = 0; i < ROWS; i++) {
+        let str = "";
+
+        for (var j = 0; j < COLS; j++)
+          str += arrayToLog[_f.xy2i(j, i)] + "  ";
+
+        console.log(str);
       }
     }
-    step(layout,i){
-      const offset = (ROWS - (layout.length / COLS)) * COLS
-      const pos = offset+i
-      if (layout[i] === 1) { //if there is a panel
+	}
 
-        const above = this.at(pos-(1*COLS))
-        const arr = [0, 1, 2, 3, 4]
-        if (above){ arr.splice(arr.indexOf(above), 1)}
-        let kinds = ss.shuffle(arr,this.rng())
-        let kind  = kinds.find((k)=> {
-          return this.matched(pos,k) === false
-        })
-        this.panels[offset+i] = kind
-      }
-    }
-    layout(){
-      return ss.shuffle(
-        [[0,0,1,0,0,0,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-         [0,0,1,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-         [1,0,1,1,1,0,1,0,1,1,1,0,1,0,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-         [0,1,1,1,0,1,0,1,1,1,0,1,0,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-         [1,1,1,0,1,1,1,1,1,0,1,1,1,1,1,0,1,1,1,1,1,0,1,1,1,1,1,0,1,1,1,1,1,0,1,1],
-         [1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,1,0,1,1,1,1,1,0,1,1,1,1,1,0,1,1,1,1,1,1,1],
-         [1,0,1,0,1,1,1,0,1,1,1,1,1,0,1,1,1,1,1,0,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1],
-         [1,0,0,0,0,0,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-         [0,0,0,1,0,0,1,0,0,1,1,1,1,0,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
-        ], this.rng())[0]
-    }
-    at(i){
-      if (this.panels[i] && (this.panels[i].kind !== null)) {
-        return this.panels[i]
-      } else {
-        return null;
-      }
-    }
-    matched(pos,kind){
-      const left  = this.at(pos-1)
-      const right = this.at(pos+1)
-      const under = this.at(pos+(1*COLS))
-      const above = this.at(pos-(1*COLS))
-
-      const left2  = this.at(pos-2)
-      const right2 = this.at(pos+2)
-      const under2 = this.at(pos+(2*COLS))
-      const above2 = this.at(pos-(2*COLS))
-      return ((left  === kind) && (right  === kind)) ||
-             ((above === kind) && (under  === kind)) ||
-             ((above === kind) && (above2 === kind)) ||
-             ((under === kind) && (under2 === kind)) ||
-             ((left  === kind) && (left2  === kind)) ||
-             ((right === kind) && (right2 === kind))
-    }
-
-
-  }
-  return controller
+	return Stack;
 }
