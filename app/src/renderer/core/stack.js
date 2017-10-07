34ce83a8c8d63863a80a3282d7a67fc7e3e99bf7
Skytrias <sotdead91@gmail.com>
2017-10-07 21:26:33 +0200

    reworked a ton to work - parameter object to set range, ground level, wells, chips

--- app/src/renderer/core/stack.js
+++ app/src/renderer/core/stack.js
@@ -15,39 +15,41 @@ module.exports = function(game) {
 	 * no combos are done once finished
 	 */
 	class Stack {
-		/**
-		 * Panels Array that saves the numbers being generated 
-		 * 
-		 * 
+		/** Panels Array that saves the numbers being generated 
 		 * @param {object} rng seed that the randomness is based on 
 		 */
 		constructor(rng) {
 			this.create = this.create.bind(this);
+			this.setArrayToSize = this.setArrayToSize.bind(this);
 			this.logArrayLikeAPlayfield = this.logArrayLikeAPlayfield.bind(this);
-		
+
 			this.rng = rng;
 			this.panels = 0;
+
+			// null block generation
+			this.amountOfSprites = [0, 1, 2, 3, 4];
+			this.wellArray = [];
+			this.chipArray = [];
 		}
 
 		/**
-     * from top to bottom creation of all playfields stack sprites
+		 * from top to bottom creation of all playfields stack sprites
      * numbers are being generated - regenerated if recent numbers are the same
-     * @param {integer} range 
-     */
-		create(range, wellPercentage) {
+		 * @param {object} object of parameters, so you now what youre changing to what 
+		 */
+		create({ range = 5, ground = 1, wells = "average", chips = "average" }) {
 			this.panels = new Array(PANELS).fill(null);
 
-			var lastNumber = -1;
-			var amountOfSprites = [0, 1, 2, 3, 4];
-
-			var amountOfWellSpace = new Array(wellPercentage).fill(0);
-			amountOfWellSpace[0] = 1;
-
+			this.wellArray = this.setArrayToSize({ noun: wells, lowest: 10, average: 20, highest: 40});
+			this.chipArray = this.setArrayToSize({ noun: chips, lowest: 2, average: 4, highest: 8 });
+			
       // fill with nulls so empty space will result in unvisible blocks
-      var savedNumbers = new Array(PANELS).fill(null);
-
-      for (var i = range * COLS; i < PANELS; i++) {
-        let currentNumber = ss.shuffle(amountOfSprites, this.rng())[0];
+      let savedNumbers = new Array(PANELS).fill(null);
+			let lastNumber = -1;
+			
+			// generate numbers from top range to bottom
+      for (var i = (PANELS - range * COLS); i < PANELS; i++) {
+        let currentNumber = ss.shuffle(this.amountOfSprites, this.rng())[0];
 
         // x and y pos to move in the array and detect things
         let indexes = _f.i2xy(i);
@@ -56,19 +58,27 @@ module.exports = function(game) {
         let topNumber = savedNumbers[_f.xy2i(indexes[0], indexes[1] - 1)];
 
 				// random chance that currentnumber is simply null
-				if (ss.shuffle(amountOfWellSpace, this.rng())[0] == 1) 
-					currentNumber = null;
-				
-					// if x = 0 we dont need to detect the left neighbor color so we set last to something different
+				if (this.wellArray.length != 1) 							// only if wellArray size has been set
+					if (i < (PANELS - ground * COLS))						// only generate nulls above ground
+						if (ss.shuffle(this.wellArray, this.rng())[0] == 1) 
+							currentNumber = null;
+
+				// random chance to have null blocks through chips sets created
+				if (this.chipArray.length != 1) 
+					if (i < PANELS - ((range - 1) * COLS))		// chips are only the top layer
+						if (ss.shuffle(this.chipArray, this.rng())[0] == 1)
+							currentNumber = null;
+
+				// if x = 0 we dont need to detect the left neighbor color so we set last to something different
 				if (indexes[0] == 0) 
 					lastNumber == -1;
 
 				// generate new number until currentNumber is unique
 				// not when its already null, do when its top / last number is same
 				while (currentNumber != null &&
-								currentNumber == topNumber ||
-								currentNumber == lastNumber) {
-					currentNumber = ss.shuffle(amountOfSprites, this.rng())[0];
+							 currentNumber == topNumber ||
+							 currentNumber == lastNumber) {
+					currentNumber = ss.shuffle(this.amountOfSprites, this.rng())[0];
 				}
 
 				// set last number and send current to array
@@ -76,16 +86,45 @@ module.exports = function(game) {
         savedNumbers[i] = currentNumber;
 			}
 
-			this.logArrayLikeAPlayfield(savedNumbers);
-
-			/* not currently working, gotta sleep
-			for (var i = PANELS; i > 0; i--) {
-				if (savedNumbers[i] == null) {
+			// nullifyBlocksAboveNulls
+			for (var i = PANELS; i > 0; i--)
+				if (savedNumbers[i] === null) 		// important === ... == caused bugs
 					savedNumbers[i - COLS] = null;
-				}
-			}*/
 
 			this.panels = savedNumbers;
+		}
+
+		/**
+		 * sets an Array to a size acording to prechosen sizes 
+		 * @param {object} noun and supposed to be array sizes, the larger the less chances
+		 * @returns {Array} 
+		 */
+		setArrayToSize({noun, lowest, average, highest}) {
+			let size = 0;
+			
+			switch (noun) {
+				case "none": break;
+			
+				case "many":
+					size = lowest; 
+					break;
+
+				case "average": 
+					size = average;
+					break;
+
+				case "few": 
+					size = highest;
+					break;
+
+				default: console.log("not existing statement sent"); break;
+			}
+
+			// set space for wellArray
+			let arr = new Array(size).fill(0);
+			arr[0] = 1;
+
+			return arr;
 		}
 
 		/**
