9812aaa0b430fae51260cf29239d8346250b72d4
Skytrias <sotdead91@gmail.com>
2017-10-11 23:37:48 +0200

    [F40] puzzle data now in puzzles.js

---
+++ app/src/renderer/core/puzzles.js
@@ -0,0 +1,39 @@
+const N = null;
+
+module.exports = {
+  all_levels: {
+    level1: {
+      panels: [
+        N, N, N, N, N, N,
+        N, N, N, N, N, N,
+        N, N, N, N, N, N,
+        N, N, N, N, N, N,
+        N, N, N, N, N, N,
+        N, N, N, N, N, N,
+        N, N, N, N, N, N,
+        N, N, 1, N, N, N,
+        N, N, 2, N, N, N,
+        N, N, 2, N, N, N,
+        N, 1, 2, 1, N, N
+      ],
+      steps: 1
+    },
+  
+    level2: {
+      panels: [
+        N, N, N, N, N, N,
+        N, N, N, N, N, N,
+        N, N, N, N, N, N,
+        N, N, N, N, N, N,
+        N, N, 5, 2, 2, N,
+        N, N, 2, 5, 0, N,
+        N, N, 4, 2, 4, N,
+        N, 3, 5, 2, 3, N,
+        N, 3, 0, 4, 0, N,
+        N, 5, 5, 3, 4, N,
+        2, 3, 2, 5, 3, 4
+      ],
+      steps: 4
+    }
+  }
+};
