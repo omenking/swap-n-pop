0c3f35d8ca13e93c65ea546633b6bd009d8068fe
Skytrias <sotdead91@gmail.com>
2017-11-22 23:58:46 +0100

    reworked how animations are added and called - temp placement of second player character

--- app/src/renderer/components/character.js
+++ app/src/renderer/components/character.js
@@ -6,41 +6,54 @@ module.exports = function(game) {
    *  you should only define the sprite animations and pos of the sprite
    */
   return class CharacterTemplate {
-    create(all_sprites, {x = 0, y = 0}, pi) {
+    create(sprite, x = 0, y = 0, pi) {
       this.playfield_num = pi;
-      this.sprite_names = all_sprites;
       this.animation_speed = 10;
       
       // sprite creation
-      this.current_sprite = game.add.sprite(x, y, this.sprite_names[0], 0);
-      this.current_sprite.anchor.setTo(0.5);
-      this.current_sprite.scale.set(2);
-      this.current_sprite.smoothed = false;
+      this.sprite = game.add.sprite(x, y, sprite, 0);
+      this.sprite.anchor.setTo(0.5);
+      this.sprite.scale.set(2);
+      this.sprite.smoothed = false;
       this.x = x;
       this.y = y;
 
-      if (this.playfield_num == 1)
-        this.current_sprite.visible = false;
-      
-      // standing animation
-      this.current_sprite.animations.add('stand');
-      this.current_sprite.animations.play('stand', this.animation_speed, true);
+      if (pi == 1) {
+        this.sprite.scale.x = -2;
+        this.x -= -60;
+      }
+
+      // adding all animations - connecting them 
+      this.add_animation("stand", 8, 0, true);
+      this.add_animation("attack", 9, 12, false, true);
+      this.add_animation("attacked", 9, 24, false, true);
+      this.add_animation("lost", 12, 36, false);
+      this.add_animation("losing", 4, 48, true);
+
+      this.sprite.play('stand');
     }
 
-    attack() {
-      this.current_sprite.loadTexture(this.sprite_names[1], 0);
-      this.current_sprite.animations.add('attack');
-      this.current_sprite.animations.play('attack', this.animation_speed, false);
-      
-      this.current_sprite.animations.getAnimation('attack').onComplete.add(() => {
-        this.current_sprite.loadTexture(this.sprite_names[0], 0);
-        this.current_sprite.play('stand', this.animation_speed, true);
-      })
+    add_animation(anim_name, hframes, offset, loop, callback = false) {
+      this.sprite.animations.add(anim_name, this.animiation_frames(hframes, offset), this.animation_speed, loop);
+
+      if (callback)
+        this.sprite.animations.getAnimation(anim_name).onComplete.add(() => {
+          this.sprite.play("stand");
+        })
+    }
+
+    animiation_frames(hframes, offset) {
+      var temp = [];
+
+      for (var i = offset; i < offset + hframes; i++)
+        temp.push(i);
+
+      return temp;
     }
 
     update() {
-      this.current_sprite.x = this.x;
-      this.current_sprite.y = this.y;
+      this.sprite.x = this.x;
+      this.sprite.y = this.y;
     }
   }
 }
