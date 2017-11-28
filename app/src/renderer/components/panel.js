a6a7c6932139e0d2a37a21f715acdf711ee2ca8b
conormackeigan <conormackeigan@gmail.com>
2017-11-28 02:23:42 -0500

    Fixed #39 for real this time

--- app/src/renderer/components/panel.js
+++ app/src/renderer/components/panel.js
@@ -187,7 +187,7 @@ module.exports = function(game){
     /** */
     get clearable() {  return this.swappable && this.under.support && !this.hidden }
     /** */
-    get comboable() {  return this.clearable || (this.state === CLEAR && this.playfield.clearing.indexOf(this) && this.swappable)}
+    get comboable() {  return this.clearable || (this.state === CLEAR && this.playfield.clearing.indexOf(this) && this.counter === 0)}
     /** */
     get empty() {      return  this.state === STATIC && this.hidden }
     /** */
