9e715a04d602af075bff2485d58b7ef098a4a28e
Rauli Laine <rauli.laine@iki.fi>
2017-12-06 00:11:29 +0200

    Export constants individually in data.ts
    File `core/data.ts` contains single `export default` which contains
    object of what appears to be constant variables. However, this `export
    default` practice prevents it from being used with ordinary `import`
    statement. Instead, it's being imported as whole and then assigned to
    constant local variables by hand, which is the incorrect way. Modify the
    file so that each constant variable is individually exported, so that
    remaining files can be converted to use proper import statements.

--- src/renderer/core/data.ts
+++ src/renderer/core/data.ts
@@ -1,98 +1,96 @@
-const N = null
-const data : any = {
-  ROWS_INV: 12,
-  ROWS_VIS: 11,
-  ROWS: 12 + 11,
-  COLS: 6,
-  PANELS: (12+11) * 6,
+export const ROWS_INV = 12;
+export const ROWS_VIS = 11;
+export const ROWS = ROWS_INV + ROWS_VIS;
+export const COLS = 6;
+export const PANELS = ROWS * COLS;
 
-  STOPTIME: 60*3,
+export const STOPTIME = 60 * 3;
 
-  //number of starting blocks
-  NRBLOCK : 17,
-  // States
-  STATIC     : Symbol('static'),
-  HANG       : Symbol('hang'),
-  FALL       : Symbol('fall'),
-  LAND       : Symbol('land'),
-  SWAP_L     : Symbol('swap_l'),
-  SWAP_R     : Symbol('swap_r'),
-  SWAPPING_L : Symbol('swaping_l'),
-  SWAPPING_R : Symbol('swaping_r'),
-  CLEAR      : Symbol('clear'),
-  POPPING    : Symbol('popping'),
-  GARBAGE    : Symbol('garbage'),
-  // Animation Frame
-  FRAME_LAND    : [4,4,4,2,2,2,3,3,3,0],
-  FRAME_CLEAR   : [5,0,5,0,5,0,5,0,5,0,5,0,
-                   5,0,5,0,5,0,5,0,5,0,5,0,
-                   5,0,5,0,5,0,5,0,5,0,5,0,
-                   5,0,5,0,5,0,5,0,6,6,6,6,
-                   6,6,6,6,6,6,6,6,6,6,6,6],
+/** Number of starting blocks. */
+export const NRBLOCK = 17;
 
-  FRAME_LIVE    :  0,
-  FRAME_DANGER  : [0,0,0,0,
-                   2,2,2,2,
-                   0,0,0,0,
-                   3,3,3,3,
-                   4,4,4,4],
+// States
+export const STATIC = Symbol('static');
+export const HANG = Symbol('hang');
+export const FALL = Symbol ('fall');
+export const LAND = Symbol('land');
+export const SWAP_L = Symbol('swap_l');
+export const SWAP_R = Symbol('swap_r');
+export const SWAPPING_L = Symbol('swaping_l');
+export const SWAPPING_R = Symbol('swaping_r');
+export const CLEAR = Symbol('clear');
+export const POPPING = Symbol('popping');
+export const GARBAGE = Symbol('garbage');
 
-  FRAME_DEAD    : 6,
-  FRAME_NEWLINE : 1,
+// Animation Frame
+export const FRAME_LAND = [4, 4, 4, 2, 2, 2, 3, 3, 3, 0];
+export const FRAME_CLEAR = [5, 0, 5, 0, 5, 0, 5, 0, 5, 0, 5, 0,
+                            5, 0, 5, 0, 5, 0, 5, 0, 5, 0, 5, 0,
+                            5, 0, 5, 0, 5, 0, 5, 0, 5, 0, 5, 0,
+                            5, 0, 5, 0, 5, 0, 5, 0, 6, 6, 6, 6,
+                            6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6];
+export const FRAME_LIVE = 0;
+export const FRAME_DANGER = [0, 0, 0, 0,
+                             2, 2, 2, 2,
+                             0, 0, 0, 0,
+                             3, 3, 3, 3,
+                             4, 4, 4, 4];
+export const FRAME_DEAD = 6;
+export const FRAME_NEWLINE = 1;
+export const FRAME_STAR = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 5, 5, 5, 5, 4, 4, 3, 3, 2, 2, 1, 1, 1, 1, 1, 1, 1];
 
-  FRAME_STAR: [1,1,2,2,3,3,4,4,5,5,5,5,5,5,4,4,3,3,2,2,1,1,1,1,1,1,1],
+// Timing
+/** The time it takes before the first panel is ready to start popping. */
+export const TIME_CLEAR = 60;
+/** When a panel is ready to pop is needs to wait for time_pop before popping. */
+export const TIME_POP = 9;
+/** How long to wait after popping last panel before panel falls. */
+export const TIME_FALL = 3;
+export const TIME_SWAP = 4;
+export const TIME_PUSH = 1000;
+/** The time the particle stays on the screen for. */
+export const TIME_PARTICLE = 20;
+export const TIME_GARBAGE_CLEAR = 30;
+export const TIME_GARBAGE_POP = 12;
 
-  // Timing
-  TIME_CLEAR : 60, //the time it takes before the first panel is ready to start popping
-  TIME_POP   : 9,  //when a panel is ready to pop is needs to wait for time_pop before popping
-  TIME_FALL  : 3,  //how long to wait after popping last panel before panel falls
-  TIME_SWAP : 4,
-  TIME_PUSH : 1000,
-  TIME_PARTICLE : 20,   // the time the particle stays on the screen for 
+export const UNIT = 32;
+export const WIN_WIDTH = 398 * 2; // 256
+export const WIN_HEIGHT = 224 * 2;
 
-  TIME_GARBAGE_CLEAR: 30,
-  TIME_GARBAGE_POP  : 12,
+export const MENUCURSORBLINK = 12;
 
-  UNIT : 32,
-  WIN_WIDTH  : 398 * 2,//256,
-  WIN_HEIGHT : 224 * 2,
+export const STARTPOS_PANELCURSOR_SPEED = 6;
 
-  MENUCURSORBLINK : 12,
-
-  STARTPOS_PANELCURSOR_SPEED : 6,
-
-  GARBAGE_SHAKE: [
-    [
-      1,1,1,
-      2,2,2,
-      1,1,1,
-      0,0,0,
-      -1,-1,-1,
-      0,0,0
-    ]
-  ],
-
-  WALL_ROLLUP: [
-    0,1,0,1,0,1,0,2,0,2,0,
-    2,0,3,3,0,3,0,4,4,0,
-    4,0,5,5,0,5,0,6,0,6,
-    0,7,7,0,7,0,0,8,0,0,
-    8,0,0,8,0,9,9,0,16,32,
-    48,64,80,96,112,128,144,160,176,192,
-    192,188,185,185,188,192,192,189,187,187,
-    189,192,192,190,189,189,190,192,192,190,
-    192,190,192,191,192,191,192,191,192
-  ],
-
-  BAUBLE_FLOAT: [
-    -1,-1,0,1,2,3,4,
-    4,5,5,6,6,7,
-    7,8,8,8,9,9,
-    9,9,9,10,10,
-    10,10,10,10,10,
-    10,10,10,10,10,
-    10,10,10,11,11,
-    11,11,11
+export const GARBAGE_SHAKE = [
+  [
+    1, 1, 1,
+    2, 2, 2,
+    1, 1, 1,
+    0, 0, 0,
+    -1, -1, -1,
+    0, 0, 0
   ]
-}
-export default data
+];
+
+export const WALL_ROLLUP = [
+  0, 1, 0, 1, 0, 1, 0, 2, 0, 2, 0,
+  2, 0, 3, 3, 0, 3, 0, 4, 4, 0,
+  4, 0, 5, 5, 0, 5, 0, 6, 0, 6,
+  0, 7, 7, 0, 7, 0, 0, 8, 0, 0,
+  8, 0, 0, 8, 0, 9, 9, 0, 16, 32,
+  48, 64, 80, 96, 112, 128, 144, 160, 176, 192,
+  192, 188, 185, 185, 188, 192, 192, 189, 187, 187,
+  189, 192, 192, 190, 189, 189, 190, 192, 192, 190,
+  192, 190, 192, 191, 192, 191, 192, 191, 192
+];
+
+export const BAUBLE_FLOAT = [
+  -1, -1, 0, 1, 2, 3, 4,
+  4, 5, 5, 6, 6, 7,
+  7, 8, 8, 8, 9, 9,
+  9, 9, 9, 10, 10,
+  10, 10, 10, 10, 10,
+  10, 10, 10, 10, 10,
+  10, 10, 10, 11, 11,
+  11, 11, 11
+];
