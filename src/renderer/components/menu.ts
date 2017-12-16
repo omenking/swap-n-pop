8b6b86a757e24a3d13e27c4ba9026a01b9955bc1
Skytrias <skytrias91@gmail.com>
2017-12-16 23:11:55 +0100

    #174 testing tween transitions

--- src/renderer/components/menu.ts
+++ src/renderer/components/menu.ts
@@ -8,6 +8,7 @@ const {ipcRenderer: ipc} = electron
 export default class ComponentMenu {
   private sprite : Phaser.Sprite
   private cursor : ComponentMenuCursor
+  private testing : Phaser.Tween
 
   constructor() {
     this.cursor = new ComponentMenuCursor()
@@ -18,12 +19,24 @@ export default class ComponentMenu {
     this.sprite = game.add.sprite(game.world.centerX, game.world.centerY, 'menu');
     this.sprite.anchor.setTo(0.5);
 
+    var spr = game.add.sprite(0, 0, "black_pixel")
+    spr.scale.setTo(game.world.width, game.world.height)
+    spr.alpha = 0
+
+    this.testing = game.add.tween(spr)
+    this.testing.to(
+      { alpha: 1 },   // properties
+      400,            // delay 
+      Phaser.Easing.Sinusoidal.Out,     // console.log(Phaser.Easing) for more options
+      true            // autostart - or start manually through this.testing.start()
+    )
+
     this.cursor.create(this, -90, -46, [
-      this.mode_1p_vs_2p_local,
-      this.mode_1p_vs_2p_online,
-      this.mode_1p_vs_cpu,
-      this.mode_improve,
-      this.mode_option
+      this.mode_1p_vs_2p_local.bind(this),
+      this.mode_1p_vs_2p_online.bind(this),
+      this.mode_1p_vs_cpu.bind(this),
+      this.mode_improve.bind(this),
+      this.mode_option.bind(this)
     ]);
   }
 
@@ -32,6 +45,7 @@ export default class ComponentMenu {
   }
 
   mode_1p_vs_2p_local() {
+    //this.testing.start() doesnt work since async state switch
     ipc.send('play-vs',{online: false, cpu: [false,false]})
   }
 
