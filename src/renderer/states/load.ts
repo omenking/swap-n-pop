1f9ac015f4808a905c53ddf25d50e1405c41ce96
Skytrias <skytrias91@gmail.com>
2017-12-25 15:15:57 +0100

    #115 rework load.ts to use json file loading, once finished load menu, remove old loading text

--- src/renderer/states/load.ts
+++ src/renderer/states/load.ts
@@ -15,65 +15,42 @@ ipc.on('asset-list', (e, files) => {
  *  Uses signals to detect once everything has been loaded correctly
  */
 export default class LoadState extends State {
-  private loader : Phaser.Text
-  private files : Phaser.Text
-  private file : Phaser.Text
   private all_assets : Array<{}>
 
   get name(): string {
     return 'load';
   }
 
+  preload() {
+    game.load.json('assets', '../src/renderer/core/assets.json');
+  }
+
   create() {
-    const x = game.world.centerX
-    const y = game.world.centerY
-    const font_style1 = { font: '20px Verdana', fill: '#FFF', align: 'center' }
-    const font_style2 = { font: '12px Verdana', fill: '#FFF', align: 'center' }
-    this.loader = game.add.text(0,y    , `Loading ${0}%`, font_style1)
-    this.files  = game.add.text(0,y+40 , "Files 0 / 0"  , font_style2)
-    this.file   = game.add.text(0,y+70 , ""             , font_style2)
-    this.loader.visible = false
-    this.files.visible  = false
-    this.file.visible   = false
-
-    game.load.onLoadStart.add(this.load_start   , this)
-    game.load.onFileComplete.add(this.file_complete, this)
-    game.load.onLoadComplete.add(this.load_complete, this)
+    this.all_assets = game.cache.getJSON('assets').assets
     
-    game.load.json('assets', 'common/assets.json');
-    console.log(game.cache.getJSON('assets'));
-    //fetch("../common/assets.json").then(r => r.json()).then(json => console.log(json))
-    this.load_all()
-  }
+    this.all_assets.forEach(e => {
+      let name : string = e["name"];
 
-  load_start() {
+      switch (e["type"]) {
+        case "music": game.load.audio(name, `./assets/music/${name}.mp3`); break;
 
-  }
+        case "sound_effects": game.load.audio(name, `./assets/sound_effects/${name}.ogg`); break;
 
-  file_complete = (progress,key,success,cur,total) => {
-    this.files.setText(`Files ${cur} / ${total}`)
-    this.file.setText(key)
-  }
+        case "image": game.load.image(name, `./assets/images/${name}.png`); break;
 
-  load_complete() {
-    controls.create()
-    game.sounds.create()
-    game.state.start('menu')
-  }
+        case "spritesheet": 
+          let props = e["options"];
+          game.load.spritesheet(name, `./assets/images/${name}.png`, props.x, props.y, props.frames);
+          break;
+      }
+    })
 
-  update() {
-    this.loader.setText(`Loading ${game.load.progress}%`);
-  }
+    game.load.onLoadComplete.add(() => { 
+      controls.create()
+      game.sounds.create()
+      game.state.start('menu')
+    }, this)
 
-  render() {
-    //this.loader.visible = true
-    //this.files.visible  = true
-    //this.file.visible   = true
-  }
-
-  load_all() {
-    
-
-
+    game.load.start();
   }
 }
