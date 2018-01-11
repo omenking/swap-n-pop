d527ba7b67c9892ded3195d67c597d4aa02773c1
conormackeigan <conormackeigan@gmail.com>
2018-01-10 22:33:41 -0500

    set chainable panels to ground if there is garbage is detected during downward column scan

--- src/renderer/components/panel.ts
+++ src/renderer/components/panel.ts
@@ -297,8 +297,12 @@ export default class ComponentPanel {
       let under = this.under
       let ground = false
       while (under !== blank) {
-          if (under.kind !== null && under.state !== LAND && under.state !== SWAP_L && under.state !== SWAPPING_L 
-          && under.state !== SWAP_R && under.state !== SWAPPING_R && under.state !== HANG && under.state !== FALL) {
+          if (under.state === GARBAGE) {
+              ground = true
+              break
+          }
+          else if (under.kind !== null && under.state !== LAND && under.state !== HANG && under.state !== FALL &&
+           under.state !== SWAP_L && under.state !== SWAPPING_L && under.state !== SWAP_R && under.state !== SWAPPING_R ) {
               ground = true
               break
           }         
